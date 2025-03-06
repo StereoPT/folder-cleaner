e3b0d91cb6e6cd34d777aada3ebe601cd9be4cac
Guido Pereira <stereopt@gmail.com>
2025-03-06 10:33:46 +0000

    feat: prepare extension for store

--- src/utils/buildException.ts
+++ src/utils/buildException.ts
@@ -1,3 +1,4 @@
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const buildException = (error: Error, message: string, meta?: Record<string, any>) => {
   return {
     error,
